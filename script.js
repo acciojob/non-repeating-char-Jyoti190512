881f92e4b2151aea00c891d1b01d398aaf2d2c3f
Jyoti190512 <134208281+Jyoti190512@users.noreply.github.com>
2023-07-15 21:48:21 +0100

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+function findFirstNonRepeatingChar() {
+  const inputString = prompt("Enter a string:");
+
+  // Loop through the string
+  for (let i = 0; i < inputString.length; i++) {
+    const char = inputString[i];
+    
+    // Check if the character is non-repeating
+    if (inputString.indexOf(char) === inputString.lastIndexOf(char)) {
+      alert(`The first non-repeating character is: ${char}`);
+      return;
+    }
+  }
+  
+  alert("No non-repeating character found in the input string.");
+}
+
+// Call the function
+findFirstNonRepeatingChar();
+
